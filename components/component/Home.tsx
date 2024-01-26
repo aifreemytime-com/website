e537ecf0b137036d9780db3fa1dada7ed7c8835d
AIFreeMyTime <aifreemytime@gmail.com>
2024-01-26 11:12:36 -0800

    Initial commit

---
+++ components/component/Home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+return (
+    <>
+      <Head>
+        <title>Learn AI Basics</title>  {/* Your custom title */}
+        <link rel="icon" href="/icon.ico" /> {/* Your favicon path */}
+      </Head>
+      {/* The rest of your page content goes here */}
+    </>
+  );
+}
+
+export default Home;
